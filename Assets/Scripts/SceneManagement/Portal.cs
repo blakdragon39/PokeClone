550ce716952861f0c3d1b50af66be68af19950b6
Blakdragon39 <jenna.hartley@live.ca>
2022-05-13 18:58:50 -0600

    Tutorial video 43

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -10,6 +10,12 @@ public class Portal : MonoBehaviour, IPlayerTriggerable {
     [SerializeField] private Transform spawnPoint;
 
     public Transform SpawnPoint => spawnPoint;
+
+    private Fader fader;
+
+    private void Start() {
+        fader = FindObjectOfType<Fader>();
+    }
     
     public void OnPlayerTriggered(PlayerController player) {
         StartCoroutine(SwitchScene(player));
@@ -19,12 +25,15 @@ public class Portal : MonoBehaviour, IPlayerTriggerable {
         DontDestroyOnLoad(gameObject);
         GameController.Instance.PauseGame(true);
         
+        yield return fader.FadeIn(0.5f);
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
         
         // find first portal in next scene with the same destination ID
         var destPortal = FindObjectsOfType<Portal>()
             .First(portal => portal != this && portal.destinationPortal == destinationPortal);
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+
+        yield return fader.FadeOut(0.5f);
         
         GameController.Instance.PauseGame(false);
         Destroy(gameObject);
